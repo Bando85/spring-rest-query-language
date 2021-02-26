02d29e03e822b5ba4c5f7f50fd75d954dc6ed6e1
Bando85 <Bando85>
2021-02-26 21:54:01 +0100

    test commit

--- src/main/java/com/baeldung/persistence/dao/UserRepository.java
+++ src/main/java/com/baeldung/persistence/dao/UserRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.baeldung.persistence.model.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long>, JpaSpecificationExecutor<User> {
 
 }
